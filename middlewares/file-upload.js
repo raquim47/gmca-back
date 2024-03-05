bad6656aa7397dc2cef1c459aaf8da53a13ff26c
[SW5] 홍승진 <cmikal47@gmail.com>
2024-03-05 16:37:05 +0900

    환경변수 설정

--- middlewares/file-upload.js
+++ middlewares/file-upload.js
@@ -3,14 +3,22 @@ const multer = require('multer');
 const path = require('path');
 const os = require('os');
 
-const serviceAccount = require(path.resolve(
-  __dirname,
-  '../config/firebase-service-account-file.json'
-));
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'gs://edu-assoc.appspot.com',
+  storageBucket: process.env.STORAGE_BUCKET
 });
 
 const bucket = admin.storage().bucket();
