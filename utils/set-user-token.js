9a3ad9224922f1ba096821420751a24042b5618e
[SW5] 홍승진 <cmikal47@gmail.com>
2024-03-05 18:57:10 +0900

    token sameSite

--- utils/set-user-token.js
+++ utils/set-user-token.js
@@ -12,7 +12,7 @@ module.exports = (res, user) => {
   res.cookie('myAssocToken', token, {
     httpOnly: true,
     secure: true,
-    sameSite: 'None',
+    sameSite: 'Lax',
     path: '/',
   });
 };
